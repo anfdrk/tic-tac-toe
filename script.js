8a50f1d5ca8debfb2cfdde90be161e82cbfe9583
Andrei Fedaruk <anfdrk@gmail.com>
2023-08-16 03:34:49 +0300

    Create a simple bot with randomized moves

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const Player = (sign) => {
-  let score = 0;
-  const getScore = () => score;
-  const incrementScore = () => score++;
-  return { sign, getScore, incrementScore }
+const Player = (sign = '', type = 'human') => {
+  const getSign = () => sign;
+  const getType = () => type;
+  return { getSign, getType };
+};
+
+const BotPlayer = (sign = '', type = 'bot') => {
+  const getSign = () => sign;
+  const getType = () => type;
+  const changeSide = () => {
+    sign = (sign === 'X') ? 'O' : 'X';
+  };
+
+  const makeMove = () => {
+    const emptyCells = GameBoard.getEmptyCells();
+    if (emptyCells.length > 0) {
+      const randomIndex = Math.floor(Math.random() * emptyCells.length);
+      GameBoard.setCell(emptyCells[randomIndex], sign);
+    }
+  };
+  
+  return { getSign, getType, changeSide, makeMove };
 };
 
 const GameBoard = (() => {
@@ -24,15 +41,29 @@ const GameBoard = (() => {
     return board.every(cell => cell !== '');
   }
 
-  return { getCell, setCell, reset, isFull };
+  function getEmptyCells() {
+    const emptyCells = [];
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        emptyCells.push(i);
+      }
+    }
+    return emptyCells;
+  }
+
+  return { getCell, setCell, reset, isFull, getEmptyCells };
 })();
 
 const ScreenController = (() => {
   const status = document.getElementById('status');
+  const gameModeSelect = document.getElementById('game-mode');
+  const sideChoser = document.querySelector('.side-chooser');
   const playerSign = document.getElementById('sign');
   const cells = document.querySelectorAll('.cell');
   const restartBtn = document.querySelector('.restart-btn');
   const winline = document.querySelector('.winline');
+  const radioX = document.getElementById('sideX');
+  const radioO = document.getElementById('sideO');
   const crossImg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <line x1="10" y1="10" x2="90" y2="90" stroke-width="20" />
     <line x1="90" y1="10" x2="10" y2="90" stroke-width="20" />
@@ -40,24 +71,51 @@ const ScreenController = (() => {
   const circleImg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <circle cx="50" cy="50" r="40" stroke-width="20" fill="none" />
   </svg>`;
+  
+  function handleCellClick(cell) {
+    const cellContent = (GameBoard.getCell(parseInt(cell.target.dataset.index)));
+    if (GameController.getIsOver() || cellContent !== '') return;
+    GameController.makeMove(parseInt(cell.target.dataset.index));
+    updateGameboard();
+  }
 
-  cells.forEach((cell) => {
-    cell.addEventListener('click', () => {
-      const cellContent = (GameBoard.getCell(parseInt(cell.dataset.index)));
-      if (GameController.isOver || cellContent !== '') return;
-      GameController.makeMove(parseInt(cell.dataset.index));
+  function handleModeSelect(e) {
+    const mode = e.target.value;
+    GameController.setMode(mode);
+    toggleSideChoser(mode);
+    winline.innerHTML = '';
+    updateGameboard();
+    playerSign.classList.remove('hidden');
+    updateStatus(GameController.getCurrentPlayer(), 'turn');
+  }
+
+  function toggleSideChoser(mode) {
+    if (mode === 'vs-bot') {
+      sideChoser.style.display = 'flex';
+      radioX.checked = true;
+    } else {
+      sideChoser.style.display = 'none';
+    }
+  }
+
+  function handleSideChose(e) {
+    if (e.target.name === 'side') {
+      GameController.setPlayerSide(e.target.value);
+      winline.innerHTML = '';
       updateGameboard();
-    });
-  });
+      playerSign.classList.remove('hidden');
+      updateStatus(GameController.getCurrentPlayer(), 'turn');
+    }
+  }
 
-  restartBtn.addEventListener('click', () => {
+  function handleRestart() {
     GameBoard.reset();
     GameController.reset();
     winline.innerHTML = '';
     updateGameboard();
     playerSign.classList.remove('hidden');
-    updateStatus('X', 'turn');
-  });
+    updateStatus(GameController.getCurrentPlayer(), 'turn');
+  }
 
   function updateGameboard() {
     for (let i = 0; i < cells.length; i++) {
@@ -125,48 +183,95 @@ const ScreenController = (() => {
     };
     return winLineCoordinates[combo.join(',')];
   }
+
+  cells.forEach((cell) => {
+    cell.addEventListener('click', handleCellClick);
+  });
+  restartBtn.addEventListener('click', handleRestart);
+  gameModeSelect.addEventListener('change', handleModeSelect);
+  sideChoser.addEventListener('change', handleSideChose);
   
   return { updateStatus, drawWinLine };
 })();
 
 const GameController = (() => {
-  const playerX = Player('X');
-  const playerO = Player('O');
+  const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  let playerX = Player('X');
+  let playerO = BotPlayer('O');
   let currentPlayer = playerX;
+  let startingPlayer = playerX;
   let isOver = false;
-  ScreenController.updateStatus(currentPlayer.sign, 'turn');
+  ScreenController.updateStatus(currentPlayer.getSign(), 'turn');
+
+  function setMode(mode) {
+    if (mode === 'vs-bot') {
+      playerX = Player('X');
+      playerO = BotPlayer('O');
+    } else {
+      playerX = Player('X');
+      playerO = Player('O');
+    }
+    currentPlayer = playerX;
+    startingPlayer = playerX;
+    isOver = false;
+    GameBoard.reset();
+  }
+
+  function setPlayerSide(selectedSide) {
+    if (selectedSide === 'x') {
+      playerX = Player('X');
+      playerO = BotPlayer('O');
+    } else {
+      playerX = BotPlayer('X');
+      playerO = Player('O');
+    }
+    currentPlayer = playerX;
+    startingPlayer = playerX;
+    GameBoard.reset();
+    if (selectedSide === 'o') {
+      currentPlayer.makeMove();
+      checkStatus();
+    }
+  }
 
   function makeMove(index) {
-    GameBoard.setCell(index, currentPlayer.sign);
+    GameBoard.setCell(index, currentPlayer.getSign());
+    checkStatus();
+    if (currentPlayer.getType() === 'bot') {
+      currentPlayer.makeMove();
+      checkStatus();
+    }
+  }
+
+  function checkStatus() {
     const winCombo = checkWin();
     if (winCombo) {
-      ScreenController.drawWinLine(winCombo, currentPlayer.sign);
-      ScreenController.updateStatus(currentPlayer.sign, 'win');
-      this.isOver = true;
+      ScreenController.drawWinLine(winCombo, currentPlayer.getSign());
+      ScreenController.updateStatus(currentPlayer.getSign(), 'win');
+      isOver = true;
       return;
     }
     if (checkDraw()) {
-      ScreenController.updateStatus(currentPlayer.sign, 'draw');
-      this.isOver = true;
+      ScreenController.updateStatus(currentPlayer.getSign(), 'draw');
+      isOver = true;
       return;
     }
     switchPlayer();
-    ScreenController.updateStatus(currentPlayer.sign, 'turn');
+    ScreenController.updateStatus(currentPlayer.getSign(), 'turn');
   }
 
   function checkWin() {
-    const winCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
     for (const combo of winCombinations) {
-      if (combo.every(index => GameBoard.getCell(index) === currentPlayer.sign)) {
+      if (combo.every(index => GameBoard.getCell(index) === currentPlayer.getSign())) {
         return combo;
       }
     }
@@ -182,9 +287,24 @@ const GameController = (() => {
   }
 
   function reset() {
-    currentPlayer = playerX;
-    this.isOver = false;
+    if (playerX.getType() === 'bot' || playerO.getType() === 'bot') {
+      currentPlayer = playerX;
+      isOver = false;
+    } else if (isOver) {
+      startingPlayer = startingPlayer === playerX ? playerO : playerX;
+      currentPlayer = startingPlayer;
+      isOver = false;
+    } else {
+      currentPlayer = startingPlayer;
+    }
+    if (currentPlayer.getType() === 'bot') {
+      currentPlayer.makeMove();
+      checkStatus();
+    }
   }
 
-  return { makeMove, reset, isOver };
+  const getIsOver = () => isOver;
+  const getCurrentPlayer = () => currentPlayer.getSign();
+
+  return { makeMove, reset, getIsOver, setMode, setPlayerSide, getCurrentPlayer };
 })();
